1fed905256ab51d4eae4a81f955966093c381792
Mehmet Samet <msametgok@hotmail.com>
2021-09-09 01:06:20 +0300

    Courses are filtered by Category

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
 
 const app = express();
 
@@ -20,10 +22,13 @@ app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //ROUTES
 app.use('/', pageRoute);
-app.use('/create', courseRoute)
+app.use('/courses', courseRoute)
+app.use('/categories', categoryRoute)
 
 const port = 3000;
 app.listen(port, () => {
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,50 +1,61 @@
-const Course = require('../models/Course')
+const Course = require('../models/Course');
+const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
-    try{
+  try {
     const course = await Course.create(req.body);
 
-        res.status(201).json({
-            status: 'success',
-            course
-        })
-    } catch(error) {
-        res.status(400).json({ 
-            status: 'fail',
-            error
-        })
-    }
+    res.status(201).json({
+      status: 'success',
+      course,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
 };
 
 exports.getAllCourses = async (req, res) => {
-    try{
-    const courses = await Course.find();
-
-        res.status(200).render('courses', {
-            courses,
-            page_name : 'courses'
-        })
-    } catch(error) {
-        res.status(400).json({ 
-            status: 'fail',
-            error
-        })
+  try {
+    const categorySlug = req.query.categories;
+    const category = await Category.findOne({slug:categorySlug});
+    console.log(category)
+    let filter = {};
+    if (categorySlug){
+        filter = { category: category._id };
     }
+
+    const courses = await Course.find(filter);
+    const categories = await Category.find();
+    res.status(200).render('courses', {
+      courses,
+      categories,
+      page_name: 'courses',
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error
+    });
+  }
 };
 
 exports.getCourse = async (req, res) => {
-    try{
-    const course = await Course.findOne({slug:req.params.slug});
+  try {
+    const course = await Course.findOne({ slug: req.params.slug });
+    const categories = await Category.find();
 
-        res.status(200).render('course', {
-            course,
-            page_name : 'courses'
-        })
-    } catch(error) {
-        res.status(400).json({ 
-            status: 'fail',
-            error
-        })
-    }
+    res.status(200).render('course', {
+      course,
+      categories,
+      page_name: 'courses',
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
 };
-
