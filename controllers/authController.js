bb040835696863c735a0667f0d16b42970c6a208
Mehmet Samet <msametgok@hotmail.com>
2021-09-10 02:07:31 +0300

    Users can enroll courses now

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const Category = require('../models/Category');
+const Course = require('../models/Course');
 const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
@@ -21,11 +23,11 @@ exports.loginUser = async (req, res) => {
     await User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
-          if (same) {
+          
             // USER SESSION
             req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
-          }
+          
         });
       }
     });
@@ -42,9 +44,13 @@ exports.logoutUser = (req, res) => {
 }
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({_id:req.session.userID})
+  const user = await User.findOne({_id:req.session.userID}).populate('courses')
+  const categories = await Category.find();
+  const courses = await Course.find({user: req.session.userID})
   res.status(200).render('dashboard', {
     page_name: "dashboard",
-    user
+    user,
+    categories,
+    courses
   });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,14 +1,17 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
-
-    res.status(201).json({
-      status: 'success',
-      course,
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID
     });
+
+    res.status(201).redirect('/courses')
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -21,12 +24,24 @@ exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
     const category = await Category.findOne({slug:categorySlug});
+    const query = req.query.search;
     let filter = {};
     if (categorySlug){
         filter = { category: category._id };
     }
-
-    const courses = await Course.find(filter);
+    if(query){
+      filter = {name: query}
+    }
+    if(!query && !categorySlug) {
+      filter.name = "",
+      filter.category = null
+    }
+    const courses = await Course.find({
+      $or: [
+        {name: { $regex: '.*' + filter.name + '.*', $options: 'i'}},
+        {category: filter.category}
+      ]
+    }).sort('-createdAt').populate('user');
     const categories = await Category.find();
     res.status(200).render('courses', {
       courses,
@@ -43,14 +58,44 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+    const user = await User.findById(req.session.userID);
+    const course = await Course.findOne({ slug: req.params.slug }).populate('user');
     const categories = await Category.find();
 
     res.status(200).render('course', {
       course,
       categories,
       page_name: 'courses',
+      user
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.enrollCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.releaseCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({_id:req.body.course_id});
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
